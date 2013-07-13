b3e85bd61d1e399b17e286405e36c1075675fcd8
Alexandre <Alexandre@macbookpro.home>
2013-07-13 21:09:10 +0200

    First commit for Apache 2.4: add ldap authz to compilation flags

---
+++ apache24.rb
@@ -0,0 +1,49 @@
+require 'formula'
+
+class Apache24 < Formula
+  homepage 'https://httpd.apache.org/'
+  url 'http://apache.mirrors.pair.com/httpd/httpd-2.4.4.tar.bz2'
+  sha1 '0c5ab7f876aa10fbe8bfab2c34f8dd3dc76db16c'
+  version '2.4.4'
+
+  skip_clean ['bin', 'sbin', 'logs']
+
+  depends_on 'pcre'
+
+  # Apache 2.4 no longer bundles apr or apr-util so we have to fetch
+  # it manually for each build
+  def fetch_apr
+    ["apr-1.4.6", "apr-util-1.4.1"].each do |tb|
+      curl "-s", "-o", "#{tb}.tar.gz", "https://www.apache.org/dist/apr/#{tb}.tar.gz"
+      system "tar -xzf #{tb}.tar.gz"
+      dir = tb.rpartition('-')[0]
+      FileUtils.mv(tb, "srclib/#{dir}")
+    end
+  end
+
+  def install
+    fetch_apr
+
+    args = [
+      "--prefix=#{prefix}",
+      "--mandir=#{man}",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--with-mpm=prefork",
+      "--with-included-apr",
+      "--enable-mods-shared=all",
+      "--enable-proxy",
+      "--enable-ssl",
+      "--enable-ldap",
+      "--enable-authnz-ldap",
+      "--with-ldap",
+      "--with-pcre=#{Formula.factory('pcre').prefix}",
+    ]
+    system './configure', *args
+    system "make"
+    system "make install"
+
+    # create logs directory
+    Dir.mkdir "#{prefix}/logs" unless File.directory? "#{prefix}/logs"
+  end
+end
