e6fe96eeaa20893394b82e1c7d5e9375bd82500b
Alexandre <Alexandre@macbookpro.home>
2013-07-14 18:17:41 +0200

    First commit of phpmyadmin v4.0.4.1

---
+++ phpmyadmin.rb
@@ -0,0 +1,57 @@
+require 'formula'
+
+def php54_installed?
+  `php -v`.match(/5\.4\./)
+end
+
+def php53_installed?
+  `php -v`.match(/5\.3\./)
+end
+
+class Phpmyadmin < Formula
+  homepage 'http://www.phpmyadmin.net'
+  url 'http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.0.4.1/phpMyAdmin-4.0.4.1-all-languages.tar.bz2'
+  sha1 'a6ac0e2b2874e1fdcf5d18a29ad01fac140f2499'
+
+  if build.include?('without-mcrypt') && MacOS.prefer_64_bit?
+    raise "64-bit machines cannot use phpmyadmin without mcrypt"
+  end
+
+  unless build.include? 'without-mcrypt'
+    depends_on 'php53-mcrypt' if php53_installed?
+    depends_on 'php54-mcrypt' if php54_installed?
+  end
+
+  unless MacOS.prefer_64_bit?
+    option 'without-mcrypt', "Exclude the php-mcrypt module"
+  end
+
+  def install
+    (share+'phpmyadmin').install Dir['*']
+  end
+
+  def caveats; <<-EOS.undent
+    Note that this formula will NOT install mysql. It is not
+    required since you might want to get connected to a remote
+    database server.
+
+    Webserver configuration example (add this at the end of
+    your /etc/apache2/httpd.conf for instance) :
+      Alias /phpmyadmin #{HOMEBREW_PREFIX}/share/phpmyadmin
+      <Directory #{HOMEBREW_PREFIX}/share/phpmyadmin/>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        Allow from all
+      </Directory>
+    Then, open http://localhost/phpmyadmin
+
+    More documentation : file://#{share}/phpmyadmin/Documentation.html
+    
+    Don't forget to copy config.sample.inc.php to config.inc.php and :
+      - change your secret blowfish
+      - uncomment the configuration lines (pma, pmapass ...)
+    
+    EOS
+  end
+end
